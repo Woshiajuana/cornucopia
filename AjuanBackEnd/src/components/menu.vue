63eaddfbd0e719341fb904cb3e3b2e92030e379d
Woshiajuana <zhigang.chen@owulia.com>
2017-07-13 15:44:55 +0800

    更新

--- AjuanBackEnd/src/components/menu.vue
+++ AjuanBackEnd/src/components/menu.vue
@@ -2,83 +2,25 @@
     <div class="menu-wrap">
         <div class="menu-inner">
             <el-menu theme="dark" router unique-opened :default-active="tabIndex" class="el-menu-vertical-demo">
-                <el-submenu index="/permission">
-                    <template slot="title"><i class="el-icon-message"></i>权限管理</template>
+                <el-submenu index="/article">
+                    <template slot="title"><i class="el-icon-message"></i>文章管理</template>
                     <el-menu-item-group>
-                        <el-menu-item index="/permission/role">角色管理</el-menu-item>
-                        <el-menu-item index="/permission/resource">资源管理</el-menu-item>
-                        <el-menu-item index="/permission/admin">管理员管理</el-menu-item>
-                        <el-menu-item index="/permission/log">管理员日志</el-menu-item>
+                        <el-menu-item index="/article?tag=all">全部文章</el-menu-item>
+                        <el-menu-item index="/article?tag=html">HTML</el-menu-item>
+                        <el-menu-item index="/article?tag=javascript">JAVASCRIPT</el-menu-item>
+                        <el-menu-item index="/article?tag=nodejs">NODEJS</el-menu-item>
+                        <el-menu-item index="/article?tag=vue">VUE</el-menu-item>
+                        <el-menu-item index="/article?tag=angular">ANGULAR</el-menu-item>
+                        <el-menu-item index="/article?tag=css">CSS</el-menu-item>
+                        <el-menu-item index="/article?tag=canvas">CANVAS</el-menu-item>
+                        <el-menu-item index="/article?tag=svg">SVG</el-menu-item>
+                        <el-menu-item index="/article?tag=java">JAVA</el-menu-item>
+                        <el-menu-item index="/article?tag=mongodb">MONGODB</el-menu-item>
+                        <el-menu-item index="/article?tag=other">OTHER</el-menu-item>
                     </el-menu-item-group>
                 </el-submenu>
-                <el-submenu index="/app">
-                    <template slot="title"><i class="el-icon-message"></i>APP权限管理</template>
-                    <el-menu-item-group>
-                        <el-menu-item index="/app/role">角色管理</el-menu-item>
-                        <el-menu-item index="/app/resource">资源管理</el-menu-item>
-                    </el-menu-item-group>
-                </el-submenu>
-                <el-submenu index="2">
-                    <template slot="title"><i class="el-icon-message"></i>入驻机构管理</template>
-                    <el-menu-item-group>
-                        <el-menu-item index="2-1">入驻代理机构列表</el-menu-item>
-                        <el-menu-item index="2-2">机构管理员列表</el-menu-item>
-                    </el-menu-item-group>
-                </el-submenu>
-                <el-submenu index="3">
-                    <template slot="title"><i class="el-icon-message"></i>产品机构管理</template>
-                    <el-menu-item-group>
-                        <el-menu-item index="3-1">产品机构列表</el-menu-item>
-                    </el-menu-item-group>
-                </el-submenu>
-                <el-submenu index="4">
-                    <template slot="title"><i class="el-icon-message"></i>APP设置</template>
-                    <el-menu-item-group>
-                        <el-menu-item index="4-1">广告位管理</el-menu-item>
-                    </el-menu-item-group>
-                </el-submenu>
-                <el-submenu index="4">
-                    <template slot="title"><i class="el-icon-message"></i>消息设置</template>
-                    <el-menu-item-group>
-                        <el-menu-item index="4-1">跑马灯设置</el-menu-item>
-                    </el-menu-item-group>
-                </el-submenu>
-                <el-submenu index="5">
-                    <template slot="title"><i class="el-icon-message"></i>版本管理</template>
-                    <el-menu-item-group>
-                        <el-menu-item index="5-1">版本记录</el-menu-item>
-                    </el-menu-item-group>
-                </el-submenu>
-                <el-menu-item index="6"><i class="el-icon-message"></i>会员列表</el-menu-item>
-                <el-submenu index="/product">
-                    <template slot="title"><i class="el-icon-message"></i>产品管理</template>
-                    <el-menu-item-group>
-                        <el-menu-item index="6-1">快贷产品管理</el-menu-item>
-                        <el-menu-item index="6-2">信用卡产品管理</el-menu-item>
-                        <el-menu-item index="6-3">信贷产品管理</el-menu-item>
-                        <el-menu-item index="/product/type">产品类型管理</el-menu-item>
-                    </el-menu-item-group>
-                </el-submenu>
-                <el-submenu index="8">
-                    <template slot="title"><i class="el-icon-message"></i>订单管理</template>
-                    <el-menu-item-group>
-                        <el-menu-item index="7-1">申卡订单</el-menu-item>
-                        <el-menu-item index="7-2">快贷订单</el-menu-item>
-                        <el-menu-item index="7-3">便民订单</el-menu-item>
-                        <el-submenu index="7-4">
-                            <template slot="title">信贷订单</template>
-                            <el-menu-item index="7-4-1">赎楼贷订单</el-menu-item>
-                        </el-submenu>
-                    </el-menu-item-group>
-                </el-submenu>
-                <el-submenu index="/system">
-                    <template slot="title"><i class="el-icon-message"></i>系统管理</template>
-                    <el-menu-item-group>
-                        <el-menu-item index="/system/institution">机构分类管理</el-menu-item>
-                        <el-menu-item index="6-2">标签管理</el-menu-item>
-                        <el-menu-item index="6-4">板块管理</el-menu-item>
-                    </el-menu-item-group>
-                </el-submenu>
+                <el-menu-item index="/tag"><i class="el-icon-message"></i>标签管理</el-menu-item>
+                <el-menu-item index="/tag"><i class="el-icon-message"></i>标签管理</el-menu-item>
             </el-menu>
         </div>
     </div>

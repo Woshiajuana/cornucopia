64aa55c18e2216213c0117391a2d901a70fc84d6
Woshiajuana <zhigang.chen@owulia.com>
2017-07-13 19:02:57 +0800

    前端页面

--- AjuanBackEnd/src/views/tag/children/GroupAddOrEdit.vue
+++ AjuanBackEnd/src/views/tag/children/GroupAddOrEdit.vue
@@ -2,13 +2,10 @@
     <div class="container-wrap">
         <div class="container-inner">
             <crumb></crumb>
-            <div class="add-role-con">
+            <div class="add-con">
                 <el-form ref="form" :model="form" label-width="80px">
-                    <el-form-item label="角色名称">
+                    <el-form-item label="分组名称">
                         <el-input v-model="form.name"></el-input>
-                    </el-form-item>
-                    <el-form-item label="描述">
-                        <el-input type="textarea" v-model="form.desc"></el-input>
                     </el-form-item>
                     <el-form-item>
                         <el-button type="primary" @click="onSubmit">确认</el-button>
--- AjuanBackEnd/src/views/tag/children/TagAddOrEdit.vue
+++ AjuanBackEnd/src/views/tag/children/TagAddOrEdit.vue
@@ -2,13 +2,16 @@
     <div class="container-wrap">
         <div class="container-inner">
             <crumb></crumb>
-            <div class="add-role-con">
+            <div class="add-con">
                 <el-form ref="form" :model="form" label-width="80px">
-                    <el-form-item label="角色名称">
+                    <el-form-item label="类别名称">
                         <el-input v-model="form.name"></el-input>
                     </el-form-item>
-                    <el-form-item label="描述">
-                        <el-input type="textarea" v-model="form.desc"></el-input>
+                    <el-form-item label="类别分组">
+                        <el-select v-model="form.name" placeholder="请选择类别分组">
+                            <el-option label="区域一" value="shanghai"></el-option>
+                            <el-option label="区域二" value="beijing"></el-option>
+                        </el-select>
                     </el-form-item>
                     <el-form-item>
                         <el-button type="primary" @click="onSubmit">确认</el-button>

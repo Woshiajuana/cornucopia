78337c8d42f57376659a9c6a9b3167425131e6fc
Woshiajuana <zhigang.chen@owulia.com>
2017-09-12 18:39:28 +0800

    更新

--- AjuanApp/src/module/navigator/navigator.js
+++ AjuanApp/src/module/navigator/navigator.js
@@ -1,10 +1,12 @@
 var navigator = weex.requireModule('navigator');
 export default {
+    /**压入*/
     push: ( { url, animated = 'true', close = '' }, params ) => {
         close && (close = 'true');
         url = params ? url + '?params=' + encodeURIComponent(JSON.stringify(params)) : url;
         navigator.push({ url: url, animated: animated, close: close }, e => {});
     },
+    /**弹出*/
     pop: (animated = 'true') => {
         navigator.pop({ animated: animated });
     }

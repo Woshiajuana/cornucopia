a3de01d70f4179acd166a2ece701110b559d2764
Woshiajuana <zhigang.chen@owulia.com>
2017-09-12 18:04:57 +0800

    测试失败  回退

--- AjuanApp/src/module/navigator/navigator.js
+++ AjuanApp/src/module/navigator/navigator.js
@@ -1,9 +1,11 @@
 var navigator = weex.requireModule('navigator');
+var modal = weex.requireModule('modal');
 export default {
     push: ( { url, animated = 'true', close = '' }, params ) => {
         close && (close = 'true');
+        modal.toast({message: 'close:'+close});
         url = params ? url + '?params=' + encodeURIComponent(JSON.stringify(params)) : url;
-        navigator.push({ url: url, animated: animated, close: close }, e => {});
+        navigator.push({ url: url, animated: animated, close: 'true' }, e => {});
     },
     pop: (animated = 'true') => {
         navigator.pop({ animated: animated });

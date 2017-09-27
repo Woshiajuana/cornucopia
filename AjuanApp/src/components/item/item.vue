6c544c8a0183a00e7fc60030980194116e0f2089
Woshiajuana <zhigang.chen@owulia.com>
2017-09-27 19:15:48 +0800

    更新

---
+++ AjuanApp/src/components/item/item.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="item-wrap">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'item',
+        props: {
+            /**左边文案*/
+            left_text: { default: '' },
+            /**左边文案*/
+            right_text: { default: '' }
+        }
+    }
+</script>
+
+<style>
+
+</style>

84e6380361890be7532c0dbc04d23c851cb924f4
Woshiajuana <zhigang.chen@owulia.com>
2020-06-04 11:55:53 +0800

    subcode code

--- src/components/suspension-panel/index.vue
+++ src/components/suspension-panel/index.vue
@@ -1,8 +1,11 @@
 <template>
 
     <div class="suspension-panel">
-        <div class="suspension-item">
-            <span class="iconfont icon-xingxing"></span>
+        <div class="suspension-item"
+             v-for="(item, index) in arrButton"
+             :title="item.title"
+             :key="index">
+            <span class="iconfont" :class="[item.icon]"></span>
         </div>
     </div>
 
@@ -12,7 +15,28 @@
     export default {
         data () {
             return {
-                arr
+                arrButton: [
+                    {
+                        title: 'GitHub',
+                        icon: 'icon-github',
+                    },
+                    {
+                        title: '微信',
+                        icon: 'icon-weixin',
+                    },
+                    {
+                        title: '发送邮件',
+                        icon: 'icon-e-mail',
+                    },
+                    {
+                        title: '分享链接',
+                        icon: 'icon-fenxiang',
+                    },
+                    {
+                        title: '回到顶部',
+                        icon: 'icon-top02',
+                    },
+                ]
             };
         },
         methods: {
@@ -22,6 +46,7 @@
 </script>
 
 <style lang="scss" scoped>
+
     @import "~src/assets/scss/define";
 
     .suspension-panel{
@@ -30,8 +55,24 @@
         @include tft(translate3d(0, -50%, 0));
         z-index: 1;
         margin-left: j(-60);
-        background-color: #6f9384;
+        /*background-color: #6f9384;*/
         width: j(40);
-        height: j(200);
     }
+    .suspension-item{
+        @extend %cp;
+        @extend %df;
+        @extend %aic;
+        @extend %jcc;
+        @extend %c6;
+        width: j(40);
+        height: j(40);
+        font-size: j(24);
+        &:hover{
+            color: $mainColor;
+        }
+        /*background-color: red;*/
+        /*border: 1px solid #ddd;*/
+        /*border-radius: j(5);*/
+    }
+
 </style>

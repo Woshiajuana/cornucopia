f1be5480e212c5f8e578abb84cc1ebeb54c48a73
Woshiajuana <zhigang.chen@owulia.com>
2020-06-04 10:10:33 +0800

    subcode code

--- src/components/suspension-panel/index.vue
+++ src/components/suspension-panel/index.vue
@@ -1,12 +1,14 @@
 <template>
     <div class="suspension-panel">
+        <div class="suspension-item">
+            <span>icon</span>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
         name: 'suspension-panel',
-
     }
 </script>
 
@@ -15,8 +17,11 @@
 
     .suspension-panel{
         @extend %pf;
+        @extend %t50;
+        @include tft(translate3d(0, -50%, 0));
         z-index: 1;
         background-color: #6f9384;
-        width: j(20);
+        width: j(50);
+        height: j(200);
     }
 </style>

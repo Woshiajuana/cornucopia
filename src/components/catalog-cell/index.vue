5a4bfad2e12cedc917045b1d1c8e56dae2eb29d9
Woshiajuana <zhigang.chen@owulia.com>
2020-06-05 14:19:37 +0800

    subcode code

---
+++ src/components/catalog-cell/index.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="catalog-cell">
+        <a class="catalog-cell-title" href="javascript:;">
+            <i></i><span>{{data.title}}</span>
+        </a>
+        <catalog-cell
+            v-if="data.children"
+            v-for="(item, index) in data.children"
+            :key="index"
+            :data="item"
+        ></catalog-cell>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'catalog-cell',
+        props: {
+            data: { default: '' },
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import "~src/assets/scss/define";
+    .catalog-cell{
+        @extend %pr;
+        .catalog-cell{
+            padding-left: j(25);
+        }
+        &.active{
+            .catalog-cell-title{
+                color: $mainColor;
+            }
+        }
+    }
+    .catalog-cell-title{
+        @extend %df;
+        @extend %cp;
+        @extend %aic;
+        @extend %c6;
+        padding: 0 j(20);
+        font-size: j(14);
+        height: j(30);
+        &:hover{
+            text-decoration: underline;
+        }
+        span{
+            @extend %df1;
+            @extend %twno;
+        }
+    }
+</style>
--- src/components/catalog-section/index.vue
+++ src/components/catalog-section/index.vue
@@ -1,26 +1,349 @@
 <template>
     <div class="catalog-section">
-
+        <div class="catalog-indicator" :style="{ top: numCurrent * 30 + 'px' }"></div>
+        <p class="catalog-title">目录</p>
+        <catalog-cell
+            v-for="(item, index) in arrCatalog"
+            :num="1"
+            :data="item"
+            :key="index">
+        </catalog-cell>
     </div>
 </template>
 
 <script>
+    import CatalogCell from 'src/components/catalog-cell'
+
     export default {
         data () {
             return {
-                numCurrent: 0,
-                arrClassify: [
-                    { label: '全部' },
-                    { label: 'HTML / CSS' },
-                    { label: 'Javascript / ES6' },
-                    { label: 'Android' },
-                    { label: 'iOS' },
-                    { label: 'Flutter' },
-                    { label: 'Nodejs' },
-                    { label: '其他' },
+                numCurrent: 2,
+                arrCatalog: [
+                    {
+                        title: '第一章',
+                        index: '0',
+                        children: [
+                            {
+                                title: '第一章第一节',
+                                index: '0-0',
+                                children: [
+                                    {
+                                        title: '第二章111111第一章1111111111111第一章1111111111111第一章11111111111111111111',
+                                        index: '0-0-0',
+                                        children: [
+                                            {
+                                                title: '第一章11111第一章111111111111111111111',
+                                                index: '0-0-0',
+                                            },
+                                            {
+                                                title: '第二章第二节'
+                                            },
+                                        ],
+                                    },
+                                ]
+                            },
+                            {
+                                title: '第一章第二节'
+                            },
+                        ],
+                    },
+                    {
+                        title: '第一章',
+                        index: '0',
+                        children: [
+                            {
+                                title: '第一章第一节',
+                                index: '0-0',
+                                children: [
+                                    {
+                                        title: '第二章111111第一章1111111111111第一章1111111111111第一章11111111111111111111',
+                                        index: '0-0-0',
+                                        children: [
+                                            {
+                                                title: '第一章11111第一章111111111111111111111',
+                                                index: '0-0-0',
+                                            },
+                                            {
+                                                title: '第二章第二节'
+                                            },
+                                        ],
+                                    },
+                                ]
+                            },
+                            {
+                                title: '第一章第二节'
+                            },
+                        ],
+                    },
+                    {
+                        title: '第一章',
+                        index: '0',
+                        children: [
+                            {
+                                title: '第一章第一节',
+                                index: '0-0',
+                                children: [
+                                    {
+                                        title: '第二章111111第一章1111111111111第一章1111111111111第一章11111111111111111111',
+                                        index: '0-0-0',
+                                        children: [
+                                            {
+                                                title: '第一章11111第一章111111111111111111111',
+                                                index: '0-0-0',
+                                            },
+                                            {
+                                                title: '第二章第二节'
+                                            },
+                                        ],
+                                    },
+                                ]
+                            },
+                            {
+                                title: '第一章第二节'
+                            },
+                        ],
+                    },
+                    {
+                        title: '第一章',
+                        index: '0',
+                        children: [
+                            {
+                                title: '第一章第一节',
+                                index: '0-0',
+                                children: [
+                                    {
+                                        title: '第二章111111第一章1111111111111第一章1111111111111第一章11111111111111111111',
+                                        index: '0-0-0',
+                                        children: [
+                                            {
+                                                title: '第一章11111第一章111111111111111111111',
+                                                index: '0-0-0',
+                                            },
+                                            {
+                                                title: '第二章第二节'
+                                            },
+                                        ],
+                                    },
+                                ]
+                            },
+                            {
+                                title: '第一章第二节'
+                            },
+                        ],
+                    },
+                    {
+                        title: '第一章',
+                        index: '0',
+                        children: [
+                            {
+                                title: '第一章第一节',
+                                index: '0-0',
+                                children: [
+                                    {
+                                        title: '第二章111111第一章1111111111111第一章1111111111111第一章11111111111111111111',
+                                        index: '0-0-0',
+                                        children: [
+                                            {
+                                                title: '第一章11111第一章111111111111111111111',
+                                                index: '0-0-0',
+                                            },
+                                            {
+                                                title: '第二章第二节'
+                                            },
+                                        ],
+                                    },
+                                ]
+                            },
+                            {
+                                title: '第一章第二节'
+                            },
+                        ],
+                    },
+                    {
+                        title: '第一章',
+                        index: '0',
+                        children: [
+                            {
+                                title: '第一章第一节',
+                                index: '0-0',
+                                children: [
+                                    {
+                                        title: '第二章111111第一章1111111111111第一章1111111111111第一章11111111111111111111',
+                                        index: '0-0-0',
+                                        children: [
+                                            {
+                                                title: '第一章11111第一章111111111111111111111',
+                                                index: '0-0-0',
+                                            },
+                                            {
+                                                title: '第二章第二节'
+                                            },
+                                        ],
+                                    },
+                                ]
+                            },
+                            {
+                                title: '第一章第二节'
+                            },
+                        ],
+                    },
+                    {
+                        title: '第一章',
+                        index: '0',
+                        children: [
+                            {
+                                title: '第一章第一节',
+                                index: '0-0',
+                                children: [
+                                    {
+                                        title: '第二章111111第一章1111111111111第一章1111111111111第一章11111111111111111111',
+                                        index: '0-0-0',
+                                        children: [
+                                            {
+                                                title: '第一章11111第一章111111111111111111111',
+                                                index: '0-0-0',
+                                            },
+                                            {
+                                                title: '第二章第二节'
+                                            },
+                                        ],
+                                    },
+                                ]
+                            },
+                            {
+                                title: '第一章第二节'
+                            },
+                        ],
+                    },
+                    {
+                        title: '第一章',
+                        index: '0',
+                        children: [
+                            {
+                                title: '第一章第一节',
+                                index: '0-0',
+                                children: [
+                                    {
+                                        title: '第二章111111第一章1111111111111第一章1111111111111第一章11111111111111111111',
+                                        index: '0-0-0',
+                                        children: [
+                                            {
+                                                title: '第一章11111第一章111111111111111111111',
+                                                index: '0-0-0',
+                                            },
+                                            {
+                                                title: '第二章第二节'
+                                            },
+                                        ],
+                                    },
+                                ]
+                            },
+                            {
+                                title: '第一章第二节'
+                            },
+                        ],
+                    },
+                    {
+                        title: '第一章',
+                        index: '0',
+                        children: [
+                            {
+                                title: '第一章第一节',
+                                index: '0-0',
+                                children: [
+                                    {
+                                        title: '第二章111111第一章1111111111111第一章1111111111111第一章11111111111111111111',
+                                        index: '0-0-0',
+                                        children: [
+                                            {
+                                                title: '第一章11111第一章111111111111111111111',
+                                                index: '0-0-0',
+                                            },
+                                            {
+                                                title: '第二章第二节'
+                                            },
+                                        ],
+                                    },
+                                ]
+                            },
+                            {
+                                title: '第一章第二节'
+                            },
+                        ],
+                    },
+                    {
+                        title: '第一章',
+                        index: '0',
+                        children: [
+                            {
+                                title: '第一章第一节',
+                                index: '0-0',
+                                children: [
+                                    {
+                                        title: '第二章111111第一章1111111111111第一章1111111111111第一章11111111111111111111',
+                                        index: '0-0-0',
+                                        children: [
+                                            {
+                                                title: '第一章11111第一章111111111111111111111',
+                                                index: '0-0-0',
+                                            },
+                                            {
+                                                title: '第二章第二节'
+                                            },
+                                        ],
+                                    },
+                                ]
+                            },
+                            {
+                                title: '第一章第二节'
+                            },
+                        ],
+                    },
+                    {
+                        title: '第一章',
+                        index: '0',
+                        children: [
+                            {
+                                title: '第一章第一节',
+                                index: '0-0',
+                                children: [
+                                    {
+                                        title: '第二章111111第一章1111111111111第一章1111111111111第一章11111111111111111111',
+                                        index: '0-0-0',
+                                        children: [
+                                            {
+                                                title: '第一章11111第一章111111111111111111111',
+                                                index: '0-0-0',
+                                            },
+                                            {
+                                                title: '第二章第二节'
+                                            },
+                                        ],
+                                    },
+                                ]
+                            },
+                            {
+                                title: '第一章第二节'
+                            },
+                        ],
+                    },
+                    {
+                        title: '第二章',
+                        children: [
+                            {
+                                title: '第二章第一节'
+                            },
+                            {
+                                title: '第二章第二节'
+                            },
+                        ],
+                    },
                 ]
             }
         },
+        components: {
+            CatalogCell,
+        }
     }
 </script>
 
@@ -28,6 +351,32 @@
     @import "~src/assets/scss/define";
     .catalog-section{
         @extend %pf;
+        @extend %b0;
+        top: j(70);
         width: j(240);
+        bottom: j(20);
+        overflow-y: auto;
+    }
+    .catalog-title{
+        @extend %fwb;
+        @extend %c3;
+        @extend %aic;
+        @extend %df;
+        padding-left: j(20);
+        height: j(30);
+        font-size: j(18);
+    }
+    .catalog-indicator{
+        @extend %cp;
+        @extend %pa;
+        @extend %r0;
+        @extend %l0;
+        @extend %t0;
+        @extend %df;
+        @extend %aic;
+        @include tst(all, 0.3s);
+        height: j(30);
+        border-left: j(4) solid $mainColor;
+        background-color: #fff;
     }
 </style>

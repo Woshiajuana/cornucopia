037c673e62b7da65dd179086a264e5ae401fc59e
Woshiajuana <zhigang.chen@owulia.com>
2020-06-23 20:41:26 +0800

    subcode code

--- cmd/ftp.cmd.js
+++ cmd/ftp.cmd.js
@@ -1,4 +1,5 @@
 
+const fs = require('fs-extra');
 const path = require('path');
 const out = require('wow-cmd').output;
 const cmdPath = process.cwd();
@@ -12,8 +13,6 @@ const Handle = (options, data, next) => {
     } = options;
     params = params ? params.toLocaleLowerCase() : '';
     try {
-        if (!data)
-            throw '未指定上传文件';
         let config = {
             host: '49.233.210.236',
             port: '21',
@@ -22,12 +21,36 @@ const Handle = (options, data, next) => {
         if (params) {
             config = Object.assign(config, require(path.join(cmdPath, params)).default);
         }
+        let { rootDir, password, entryDir } = config;
+        if (entryDir) {
+            entryDir = path.join(cmdPath, entryDir);
+            data = [];
+            ;(function walk(directory) {
+                fs.readdirSync(directory).forEach((file) => {
+                    const fullPath = path.join(directory, file);
+                    const fileStat = fs.statSync(fullPath);
+                    const fileRelativePath = fullPath.replace(entryDir, '').replace(/\\/g, '/');
+                    const fileDirArr = fileRelativePath.split('\/');
+                    const fileLastDir = fileDirArr[fileDirArr.length - 1];
+                    if (fileStat.isFile()) {
+                        fileDirArr.shift();
+                        data.push({
+                            input: fullPath,
+                            output: `${fileRelativePath.substring(1)}`,
+                        });
+                    } else if (fileStat.isDirectory() && [].indexOf(fileLastDir) === -1) {
+                        walk(fullPath);
+                    }
+                });
+            })(entryDir);
+        }
+        if (!data)
+            throw '未指定上传文件';
         let fire = function * () {
-            let { rootDir, password } = config;
             if (!rootDir) {
                 rootDir = yield prompt('\n请输入上传根目录(不要斜杆开头结尾): ');
             }
-            if (['ajuan', 'jianyi', 'tiaotiao'].indexOf(rootDir) === -1) {
+            if ([ 'jianyi', 'ajuan', 'tiaotiao'].indexOf(rootDir) === -1) {
                 out.error('ftp.cmd=>', `上传根目录错误`);
                 return process.exit(0);
             }

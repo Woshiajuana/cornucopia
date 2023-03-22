81687758dae00dd8d544ba9d4b80be4a4edddc79
chenzhigang <979703986@qq.com>
2023-03-22 17:27:46 +0800

    chore: clearn project

--- cmd/config.js
+++
@@ -1,8 +0,0 @@
-
-export default {
-    entryDir: 'static', // 上传目录
-    inputDir: 'static/articles', // 遍历文章目录 可修改
-    outputDir: 'static/mocks', // mocks 数据生成目录
-    rootDir: 'jianyi', // 上传FTP 服务器 根目录 配置了这个之后就后续上传的时候就无需实时填写（不要斜杆开头结尾）
-    // 不要配置在此ftp密码
-}

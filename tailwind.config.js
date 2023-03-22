0b054c6fdf1393fc9dc98b0d3a1a4481a3bf623a
chenzhigang <979703986@qq.com>
2023-03-22 18:12:20 +0800

    chore: 依赖包

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
+function withOpacity(cssVariable) {
+  return ({ opacityValue }) => {
+    return `rgba(var(${cssVariable}), ${opacityValue})`
+  }
+}
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },

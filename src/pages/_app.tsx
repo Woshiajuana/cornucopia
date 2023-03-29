1e09b9e3aa2855cebed3570ad158f5729e3a8265
chenzhigang <979703986@qq.com>
2023-03-29 17:27:48 +0800

    chore: nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
 import type { AppProps } from 'next/app'
 import { Layout } from '@/layouts'
+import { setupNprogress } from '@/utils'
 import '@/assets/scss/globals.scss'
+
+// 设置loading
+setupNprogress()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
---
+++ src/utils/nprogress.ts
@@ -0,0 +1,16 @@
+import { Router } from 'next/router'
+import np from 'nprogress'
+import 'nprogress/nprogress.css'
+
+np.configure({
+  showSpinner: false,
+})
+
+export const nprogress = np
+
+export function setupNprogress() {
+  //Binding events.
+  Router.events.on('routeChangeStart', () => nprogress.start())
+  Router.events.on('routeChangeComplete', () => nprogress.done())
+  Router.events.on('routeChangeError', () => nprogress.done())
+}

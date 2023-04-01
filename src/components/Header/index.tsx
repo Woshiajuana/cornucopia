5eea309e7ae691dbce1869e641a025e61717d259
chenzhigang <979703986@qq.com>
2023-04-01 22:33:23 +0800

    chore: header sidebar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,13 +6,9 @@ import Link from 'next/link'
 export function Header() {
   const { resolvedTheme, setTheme } = useTheme()
   const mounted = useMounted()
-  const handleSwitchTheme = () => {
-    console.log(1)
-    setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')
-  }
 
   return (
-    <header className="sticky top-0 z-10 bg-white/60 bg-clip-padding backdrop-blur-xl">
+    <header className="sticky top-0 z-10 bg-white/60 dark:bg-black/60 bg-clip-padding backdrop-blur-xl">
       <div className="flex items-center justify-between h-14 max-w-main m-auto box-border px-4">
         <Link scroll={false} href="/" className="flex items-center text-lg">
           Ajuan Blog
@@ -20,7 +16,7 @@ export function Header() {
         <button
           aria-label="Toggle Theme"
           className="w-10 h-10 flex items-center justify-center hover:bg-gray-100 rounded transition-colors"
-          onClick={handleSwitchTheme}
+          onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
         >
           <Icon name={mounted ? resolvedTheme : 'light'} />
         </button>
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,14 +1,25 @@
 import { Icon } from '@/components'
 import { useRouter } from 'next/router'
-import { useDocumentEle, useScrolling, useScrollTo, useWindow } from '@/hooks'
+import {
+  useDocumentEle,
+  useMounted,
+  useScrolling,
+  useScrollTo,
+  useWindow,
+} from '@/hooks'
 import { useState } from 'react'
+import { useTheme } from 'next-themes'
+
+export interface SidebarProps {
+  className?: string
+}
 
 export function Sidebar() {
   const router = useRouter()
 
+  // 返回
   const documentEle = useDocumentEle()
   const handleReturn = useScrollTo(documentEle)
-
   const windowRef = useWindow()
   const [visible, setVisible] = useState(false)
   useScrolling(windowRef, (event) => {
@@ -19,14 +30,27 @@ export function Sidebar() {
     }
   })
 
+  // 切换主题
+  const { resolvedTheme, setTheme } = useTheme()
+  const mounted = useMounted()
+
   return (
     <div className="fixed -ml-14 top-52">
       <button
+        aria-label="Return"
         title="返回"
         onClick={() => router.back()}
         className="w-10 h-10 flex items-center justify-center cursor-pointer transition-colors hover:bg-gray-100 text-gray-500 hover:text-gray-900 rounded"
       >
         <Icon size="24" name="return" />
+      </button>
+      <button
+        className="mt-1 w-10 h-10 flex items-center justify-center cursor-pointer transition-colors hover:bg-gray-100 text-gray-500 hover:text-gray-900 rounded"
+        aria-label="Toggle Theme"
+        title="切换主题"
+        onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
+      >
+        <Icon size={24} name={mounted ? resolvedTheme : 'light'} />
       </button>
       <a
         title="GitHub"
@@ -43,12 +67,12 @@ export function Sidebar() {
       >
         <Icon size="24" name="mail" />
       </a>
-      <div
+      <button
         title="分享链接"
         className="mt-1 w-10 h-10 flex items-center justify-center cursor-pointer transition-colors hover:bg-gray-100 text-gray-500 hover:text-gray-900 rounded"
       >
         <Icon size="24" name="link" />
-      </div>
+      </button>
       {visible && (
         <button
           onClick={() => handleReturn(0)}

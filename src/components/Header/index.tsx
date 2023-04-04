7ba1977f0be4d9f32ccf636e0ddb7b28fe450e2b
chenzhigang <979703986@qq.com>
2023-04-04 14:13:24 +0800

    chore: header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,7 +6,7 @@ export function Header() {
     <header className="sticky top-0 z-10 bg-white/60 dark:bg-black/60 bg-clip-padding backdrop-blur-xl">
       <div className="flex items-center justify-between h-14 max-w-main m-auto box-border px-4">
         <Link scroll={false} href="/" className="flex items-center text-lg">
-          Ajuan Blog
+          Ajuan Blog 11
         </Link>
 
         <SearchBox />
